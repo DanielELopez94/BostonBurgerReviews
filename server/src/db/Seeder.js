07d92f70aaa331fd96338b1fc7c0ceb03a33c53b
Jack Marini <jackmarini@Jacks-MacBook-Air.local>
2022-05-03 14:24:15 -0400

    createRestaurantTables

--- server/src/db/Seeder.js
+++ server/src/db/Seeder.js
@@ -1,13 +1,15 @@
 /* eslint-disable no-console */
-import { connection } from "../boot.js"
+import RestaurantSeeder from "./seeders/RestaurantSeeders.js";
+import { connection } from "../boot.js";
 
 class Seeder {
   static async seed() {
-    // include individual seed commands here
+    console.log("seeding restaurants...");
+    await RestaurantSeeder.seed();
 
-    console.log("Done!")
-    await connection.destroy()
+    console.log("Done!");
+    await connection.destroy();
   }
 }
 
-export default Seeder
+export default Seeder;
