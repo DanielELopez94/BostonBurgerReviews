d6ef89767e4ebaa1d4ecff88d4115073e7bcefd3
Aaron Alexius <aaronalexius@gmail.com>
2022-05-03 16:23:47 -0400

    Authenticates Users: registration, sign in, and sign out.  Patches format: email bug using regex

--- server/src/routes/api/v1/usersRouter.js
+++ server/src/routes/api/v1/usersRouter.js
@@ -5,15 +5,18 @@ import { User } from "../../../models/index.js";
 const usersRouter = new express.Router();
 
 usersRouter.post("/", async (req, res) => {
+  console.log("raw body", req.body)
   const { email, password, passwordConfirmation } = req.body;
+
   try {
     const persistedUser = await User.query().insertAndFetch({ email, password });
+    console.log("Persisted User", persistedUser)
     return req.login(persistedUser, () => {
       return res.status(201).json({ user: persistedUser });
     });
   } catch (error) {
     console.log(error);
-    return res.status(422).json({ errors: error });
+    return res.status(500).json({ errors: error })
   }
 });
 
