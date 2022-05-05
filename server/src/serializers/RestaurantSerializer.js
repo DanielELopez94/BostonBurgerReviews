41124b7320c43cd9c0a54568428f6bd385725df4
Aaron Alexius <aaronalexius@gmail.com>
2022-05-05 17:25:13 -0400

    Corrects indentation on RestaurantSerializer

--- server/src/serializers/RestaurantSerializer.js
+++ server/src/serializers/RestaurantSerializer.js
@@ -12,20 +12,20 @@ class RestaurantSerializer {
 
   static async getDetail(restaurant) {
     try {
-    const allowedAttributes = ["id", "name", "location"]
-    let serializedRestaurant = {}
+      const allowedAttributes = ["id", "name", "location"]
+      let serializedRestaurant = {}
 
-    for (const attribute of allowedAttributes) {
-      serializedRestaurant[attribute] = restaurant[attribute]
-    }
-    const relatedBurgers = await restaurant.$relatedQuery("burgers")
-    const serializedBurgers = await Promise.all(
-      relatedBurgers.map(async (burger) => {
-        return BurgerSerializer.getSummary(burger)
-      })
-    )
-    serializedRestaurant.burgers = serializedBurgers
-    return serializedRestaurant
+      for (const attribute of allowedAttributes) {
+        serializedRestaurant[attribute] = restaurant[attribute]
+      }
+      const relatedBurgers = await restaurant.$relatedQuery("burgers")
+      const serializedBurgers = await Promise.all(
+        relatedBurgers.map(async (burger) => {
+          return BurgerSerializer.getSummary(burger)
+        })
+      )
+      serializedRestaurant.burgers = serializedBurgers
+      return serializedRestaurant
     } catch (error) {
       throw error
     }
