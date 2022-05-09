85d215a05f4b9ed4cdaf03194c0d6d1d2692e422
Aaron Alexius <aaronalexius@gmail.com>
2022-05-09 13:53:23 -0400

    Merge branch 'main' of https://github.com/DanielELopez94/BostonBurgerReviews into indexStyling

---
+++ client/src/components/BurgerShowPage.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react"
+
+const BurgerShowPage = props => {
+  const [burger, setBurger] = useState({
+    name: "", 
+    vegetarian: null
+  })
+  const getBurger = async () => {
+      const burgerId = props.match.params.id
+    try {
+      const response = await fetch(`/api/v1/burgers/${burgerId}`)
+      if (!response.ok) {
+        const errorMessage = `${response.status} (${response.statusText})`
+        const error = new Error(errorMessage)
+        throw(error)
+      }
+      const body = await response.json() 
+      setBurger(body.burger)
+    }catch (error) {
+      console.error(`Error in fetch: ${error.message}`)
+    }
+  }
+
+  useEffect(()=> {
+    getBurger()
+  }, [])
+
+  return (
+    <div>
+      <h2>{burger.name}</h2>
+      <h2>{burger.vegetarian}</h2>
+    </div>
+  )
+  
+}
+
+export default BurgerShowPage
